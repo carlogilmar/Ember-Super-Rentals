4d4b0d168fac8dc943a9efb27f53a524f57084f8
carlogilmar <carlogilmar12@gmail.com>
2018-12-11 23:22:41 -0600

    3/RoutesAndTemplates Pass second test: should link to information about the company.

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
@@ -10,10 +10,13 @@ module('Acceptance | list rentals', function(hooks) {
     assert.equal(currentURL(), '/rentals');
   });
 
-  //test('should link to information about the company.', async function (assert) {
-  //});
+	test('Test 2: should link to information about the company.', async function (assert) {
+		await visit('/');
+		await click(".menu-about");
+		assert.equal(currentURL(), '/about', 'should navigate to about');
+	});
 
-  //test('should link to contact information.', async function (assert) {
+ //test('should link to contact information.', async function (assert) {
   //});
 
   //test('should list available rentals.', async function (assert) {

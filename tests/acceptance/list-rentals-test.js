39518bfaca40e782b014c0f32e56f7b7e04ccd08
carlogilmar <carlogilmar12@gmail.com>
2018-12-11 23:07:07 -0600

    2/Planning Write the test for all the requirements in this tutorial

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -9,4 +9,23 @@ module('Acceptance | list rentals', function(hooks) {
     await visit('/');
     assert.equal(currentURL(), '/');
   });
+
+  test('should show rentals as the home page', async function (assert) {
+  });
+
+  test('should link to information about the company.', async function (assert) {
+  });
+
+  test('should link to contact information.', async function (assert) {
+  });
+
+  test('should list available rentals.', async function (assert) {
+  });
+
+  test('should filter the list of rentals by city.', async function (assert) {
+  });
+
+  test('should show details for a selected rental', async function (assert) {
+  });
+
 });

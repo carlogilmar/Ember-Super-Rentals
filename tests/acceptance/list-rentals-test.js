9cfb574ee7e9a27a6e02ccacbe9e5525fc5d6024
carlogilmar <carlogilmar12@gmail.com>
2018-12-11 23:19:15 -0600

    3/RoutesAndTemplates Pass first test: should show rentals as the home page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,27 +5,24 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('Acceptance Test 1: visiting / index', async function(assert) {
+  test('Test 1: Should show rentals as the home page', async function (assert) {
     await visit('/');
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/rentals');
   });
 
-  test('should show rentals as the home page', async function (assert) {
-  });
+  //test('should link to information about the company.', async function (assert) {
+  //});
 
-  test('should link to information about the company.', async function (assert) {
-  });
+  //test('should link to contact information.', async function (assert) {
+  //});
 
-  test('should link to contact information.', async function (assert) {
-  });
+  //test('should list available rentals.', async function (assert) {
+  //});
 
-  test('should list available rentals.', async function (assert) {
-  });
+  //test('should filter the list of rentals by city.', async function (assert) {
+  //});
 
-  test('should filter the list of rentals by city.', async function (assert) {
-  });
-
-  test('should show details for a selected rental', async function (assert) {
-  });
+  //test('should show details for a selected rental', async function (assert) {
+  //});
 
 });

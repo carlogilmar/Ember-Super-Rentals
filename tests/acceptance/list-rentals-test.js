1566957862b594aa831d736d1634ba549473b139
carlogilmar <carlogilmar12@gmail.com>
2018-12-11 23:05:49 -0600

    2/Planning Write a first test for visit index

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,9 +5,8 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /list-rentals', async function(assert) {
-    await visit('/list-rentals');
-
-    assert.equal(currentURL(), '/list-rentals');
+  test('Acceptance Test 1: visiting / index', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/');
   });
 });

7b2776e63adba0f69c1fc56bb4000a4b59a55aaf
carlogilmar <carlogilmar12@gmail.com>
2018-12-20 22:51:13 -0600

    10/Services Inject service mapElement in location-map component

--- app/components/location-map.js
+++ app/components/location-map.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  classNames: ['map-container'],
+  mapElement: service(),
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.mapElement.getMapElement(this.location).then((mapElement) => {
+      this.element.append(mapElement);
+    });
+
+  }
 });

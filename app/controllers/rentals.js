bc07a1f0bb8af7a84e2c6d7c8373842130a283c5
carlogilmar <carlogilmar12@gmail.com>
2018-12-20 22:18:08 -0600

    9/ComplexComponent Adding validation in controller

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,4 +1,11 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  filterByCity(param) {
+    if (param !== '') {
+      return this.store.query('rental', { city: param });
+    } else {
+      return this.store.findAll('rental');
+    }
+  }
 });

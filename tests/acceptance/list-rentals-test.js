5165f5c1c4d6ae725deb7a767558f29778d57f4c
carlogilmar <carlogilmar12@gmail.com>
2018-12-20 22:13:05 -0600

    8/EmberData Updating acceptance test for include mirage config

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,9 +1,11 @@
 import { module, test } from 'qunit';
 import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('Test 1: Should show rentals as the home page', async function (assert) {
     await visit('/');

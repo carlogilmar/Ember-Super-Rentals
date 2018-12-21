de25ce9780276690974abb56f839eade69d13377
carlogilmar <carlogilmar12@gmail.com>
2018-12-20 22:35:41 -0600

    9/ComplexComponent Adding protective code to prevent async execution in controller

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,13 +1,21 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+
   actions: {
     filterByCity(param) {
       if (param !== '') {
-        return this.store.query('rental', { city: param });
+        return this.store
+          .query('rental', { city: param }).then((results) => {
+            return { query: param, results: results };
+          });
       } else {
-        return this.store.findAll('rental');
+        return this.store
+          .findAll('rental').then((results) => {
+            return { query: param, results: results };
+          });
       }
     }
   }
+
 });

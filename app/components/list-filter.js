27dcc08cdd44a4f5eef611b60bdbc9dc2f114bcc
carlogilmar <carlogilmar12@gmail.com>
2018-12-20 22:38:07 -0600

    9/ComplexComponent Adding protective code to prevent async execution in list-filter component

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -6,14 +6,20 @@ export default Component.extend({
 
 	init() {
 		this._super(...arguments);
-		this.filter('').then((results) => this.set('results', results));
+		this.filter('').then((allResults) => {
+			this.set('results', allResults.results);
+		});
 	},
 
 	actions: {
 		handleFilterEntry() {
 			let filterInputValue = this.value;
 			let filterAction = this.filter;
-			filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+			filterAction(filterInputValue).then((filterResults) => {
+				if (filterResults.query === this.value) {
+					this.set('results', filterResults.results);
+				}
+			});
 		}
 	}
 });

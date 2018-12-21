f495e17495991e729f56b196beddcf698424880f
carlogilmar <carlogilmar12@gmail.com>
2018-12-20 22:41:46 -0600

    9/ComplexComponent Update integration test for list-filter component

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -1,26 +1,49 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import {
+  click,
+  currentURL,
+  visit,
+  fillIn,
+  triggerKeyEvent } from '@ember/test-helpers'
 import hbs from 'htmlbars-inline-precompile';
+import { resolve } from 'rsvp';
+
+const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francisco'}];
 
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{list-filter}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('should initially load all listings', async function (assert) {
+    // we want our actions to return promises,
+    //since they are potentially fetching data asynchronously
+    this.set('filterByCity', () => resolve({ results: ITEMS }));
+    // with an integration test,
+    // you can set up and use your component in the same way your application
+    // will use it.
     await render(hbs`
-      {{#list-filter}}
-        template block text
+      {{#list-filter filter=(action filterByCity) as |results|}}
+      <ul>
+      {{#each results as |item|}}
+      <li class="city">
+      {{item.city}}
+      </li>
+      {{/each}}
+      </ul>
       {{/list-filter}}
-    `);
+      `);
+    return settled().then(() => {
+      assert.equal(this.element.querySelectorAll('.city').length, 3);
+      assert.equal(this.element.querySelector('.city').textContent.trim(), 'San Francisco');
+    });
+  });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
 });

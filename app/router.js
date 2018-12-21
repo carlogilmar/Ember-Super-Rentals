908ac7d2fa8c8db129458169e0fe64cc14a85728
carlogilmar <carlogilmar12@gmail.com>
2018-12-20 22:59:03 -0600

    11/NestedRoutes Adding an index nested route from rentals route

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('rentals');
+  this.route('rentals', function() {});
 });
 
 export default Router;
